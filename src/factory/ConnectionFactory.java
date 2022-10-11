3bd3699baa3447421ca508629513f4597959ece3
giboxz <gilvanfsfilho@fac.pe.senac.br>
2022-10-11 18:30:07 -0300

    Arrumando detalhes de conexão

--- src/factory/ConnectionFactory.java
+++ src/factory/ConnectionFactory.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-	private static final String USUARIO = "root";
-	private static final String SENHA = "root";
+	private static final String USUARIO = "seu_usuario_mysql";
+	private static final String SENHA = "sua_senha_mysql";
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/leagueofjava_db";
 	
 	public Connection recuperarConexao() {
